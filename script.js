1a877d1a4db1677ed365b0210989cb3bfe1f6bf9
NEERAJ023 <143694023+NEERAJ023@users.noreply.github.com>
2023-11-08 20:45:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let browserName= navigator.appName;
+let version=navigator.appVersion;
+
+let div=document.querySelector("#browser-info");
+let content=document.createTextNode("You are using " + browserName + " version " + version);
+div.appendChild(content);
